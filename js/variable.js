808617591d4c12a56d66e7b5afde3a5fb2608bdf
Poran chowdury <chowdury659@gmail.com>
2020-07-31 10:07:36 +0600

    first commit

---
+++ js/variable.js
@@ -0,0 +1,12 @@
+
+let name = 'poran';
+console.log(name);
+
+// can not be resrved keyword
+// should be meaningful
+// can not start with a number
+// cant not containt a space or hyphen(-)
+
+let fristName;
+//camele notatation
+let intersRate = 30;
