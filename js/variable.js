fe308a60d02117a723b4838f58c0965a6a589535
Poran chowdury <chowdury659@gmail.com>
2020-07-31 20:34:21 +0600

    opatator

--- js/variable.js
+++ js/variable.js
@@ -56,3 +56,31 @@ function square(number){
 }
 let number = square(2);
 console.log(number);
+
+//oparator 
+let x = 10;
+//incriment oparator
+x++;
+//
+x += 5;
+x *=5;
+// comparision oparator
+
+let y = 1;
+console.log(y> 0);
+console.log(y< 0);
+console.log(y<=0);
+// equal oparator
+console.log(y === 1);
+console.log(y !==1);
+// strict equality 
+console.log(1 === 1);
+//lose equality  b
+console.log('1' == 1);
+
+// ternary oparator 
+let points = 110 ;
+let type = points > 100 ?'gold' : 'silver';
+console.log(type);
+
+
