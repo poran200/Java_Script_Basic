1ffdcf1cdeb3bc99b98d350d9786fd8086d28e1f
Poran chowdury <chowdury659@gmail.com>
2020-08-17 23:16:48 +0600

    object factory

--- js/exersice/exeFunction.js
+++ js/exersice/exeFunction.js
@@ -8,7 +8,7 @@ const makeDeck = {
     for (const value of values.split(",")) {
       for (const suit of suits) {
         deck.push({
-           value,
+          value,
           suit,
         });
       }
@@ -27,6 +27,40 @@ const makeDeck = {
     return cards;
   },
 };
-const mydeck = makeDeck.initializeDeck();
-const card = makeDeck.drawCard();
-console.log(card);
+
+// deck factory
+const deckfactory = () => {
+  return {
+    deck: [],
+    drawnCard: [],
+    suits: ["hearts", "diamonds", "spades", "clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    initializeDeck() {
+      const { suits, values, deck } = this;
+      for (const value of values.split(",")) {
+        for (const suit of suits) {
+          deck.push({
+            value,
+            suit,
+          });
+        }
+      }
+    },
+    drawCard() {
+      const card = this.deck.pop();
+      this.drawnCard.push(card);
+      return card;
+    },
+    drawMultipule(numCard) {
+      const cards = [];
+      for (let i = 0; i < numCard; i++) {
+        cards.push(this.drawCard());
+      }
+      return cards;
+    },
+  };
+};
+const mydeck = deckfactory();
+mydeck.initializeDeck();
+const card = mydeck.drawCard();
+console.log(mydeck.drawMultipule(2));
